3d5c4793d58d387ac0e3b96217e4402ac4c94b0d
AscendedHobo <alanwelch91@gmail.com>
2024-11-22 16:30:10 +0000

    purging

--- Multi table list.py
+++
@@ -1,9 +0,0 @@
-
-
-user_number = int(input("insert your number to be multiplied :"))
-
-listnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-for x in listnumbers:
-
-    print(f"{x} * {user_number} = {x * user_number}")
--- test2.py
+++
@@ -1,6 +0,0 @@
-
-for x in range(-10,10):
-    y = 2.5 * x + 5
-    print(x, "\t" , y)
-
-print("end")
