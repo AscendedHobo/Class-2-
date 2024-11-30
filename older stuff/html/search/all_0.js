19dab429d7fa7ae64a996616120fc56e34e4cbc3
Alan <alanwelch91@gmail.com>
2024-11-30 11:36:06 +0000

    deletion

--- older stuff/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['class_0',['Class',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- older stuff/html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['teach_0',['Teach',['../namespace_teach.html',1,'']]],
-  ['type_1',['Type',['../namespace_type.html',1,'']]]
-];
